2dd425d25997eb067ddeb75b8daec582e15082dd
martin f. krafft <madduck@madduck.net>
2025-12-25 22:26:07 +0100

    fix: annotate KeyAndFunc map in cli.debug

--- tcboard/cli/debug.py
+++ tcboard/cli/debug.py
@@ -4,7 +4,10 @@ from contextlib import asynccontextmanager
 from functools import partial
 
 from click_async_plugins import PluginLifespan, plugin
-from click_async_plugins.debug import monitor_stdin_for_debug_commands
+from click_async_plugins.debug import (
+    KeyCmdMapType,
+    monitor_stdin_for_debug_commands,
+)
 from tptools.util import nonblocking_write
 
 from .util import CliContext, pass_clictx
@@ -14,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def debug_key_press_handler(clictx: CliContext) -> PluginLifespan:
-    key_to_cmd = {}
+    key_to_cmd: KeyCmdMapType[CliContext] = {}
     puts = partial(nonblocking_write, file=sys.stderr, eol="\n")
     async with monitor_stdin_for_debug_commands(
         clictx, key_to_cmd=key_to_cmd, puts=puts
@@ -29,3 +32,6 @@ async def debug(clictx: CliContext) -> PluginLifespan:
 
     async with debug_key_press_handler(clictx) as task:
         yield task
+    key_to_cmd: KeyCmdMapType[CliContext] = {
+        0x0C: KeyAndFunc("^L", list_connections),
+    }
